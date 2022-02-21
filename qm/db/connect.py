45361a26ba0b5bd1df61f75104bfe0cfcd8bcda9
jungwon-moon <jwoung0@naver.com>
2022-02-22 08:20:39 +0900

    postgres connect 함수 추가 이후 수정 필요

--- qm/db/connect.py
+++ qm/db/connect.py
@@ -2,7 +2,9 @@ import os
 import sys
 import json
 from pathlib import Path
+from qm.db.db_class import DB
 from pymongo import MongoClient
+import psycopg2
 
 SECRET_PATH = Path(__file__).resolve().parent
 SECRET_FILE = SECRET_PATH / 'db.json'
@@ -20,5 +22,19 @@ def mongodb_connect(host=None, port=27017):
                 host = value
 
     db = MongoClient(host, port)
+
+    return db
+
+
+def postgres_connect(host=None, port=5432):
+
+    if os.path.isfile(SECRET_FILE) is True:
+        secrets = json.loads(open(SECRET_FILE).read())
+        for key, value in secrets.items():
+            if key == 'gcp':
+                info = DB(value)
+
+    db = psycopg2.connect(host=info.host, dbname=info.dbname,
+                          password=info.password, port=port)
 
     return db
