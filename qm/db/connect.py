2ff95e133f89d32da5305962a3c25c5f849ca767
jungwon-moon <jwoung0@naver.com>
2022-02-22 08:53:48 +0900

    connect 수정

--- qm/db/connect.py
+++ qm/db/connect.py
@@ -10,31 +10,15 @@ SECRET_PATH = Path(__file__).resolve().parent
 SECRET_FILE = SECRET_PATH / 'db.json'
 
 
-def mongodb_connect(host=None, port=27017):
-
-    if host is not None:
-        pass
-
-    elif os.path.isfile(SECRET_FILE) is True:
-        secrets = json.loads(open(SECRET_FILE).read())
-        for key, value in secrets.items():
-            if key == 'host':
-                host = value
-
-    db = MongoClient(host, port)
+def mongodb_connect(properties):
+    db = DB(properties)
+    db = MongoClient(db.host, db.port)
 
     return db
 
 
-def postgres_connect(host=None, port=5432):
-
-    if os.path.isfile(SECRET_FILE) is True:
-        secrets = json.loads(open(SECRET_FILE).read())
-        for key, value in secrets.items():
-            if key == 'gcp':
-                info = DB(value)
-
-    db = psycopg2.connect(host=info.host, dbname=info.dbname,
-                          password=info.password, port=port)
-
-    return db
+def postgres_connect(properties):
+    db = DB(properties)
+    pgdb = psycopg2.connect(host=db.host, dbname=db.dbname,
+                            password=db.password, port=db.port)
+    return pgdb
