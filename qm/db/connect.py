779ca154a41cfb5eb5549f695c1ff955b273ae55
jungwon-moon <jwoung0@naver.com>
2022-02-25 06:06:41 +0900

    테스트 코드 제거

--- qm/db/connect.py
+++ qm/db/connect.py
@@ -1,14 +1,7 @@
-import os
-import sys
-import json
 from pathlib import Path
 from qm.db.DB import POSTGRESCRUD
 from pymongo import MongoClient
-import psycopg2
 
-SECRET_PATH = Path(__file__).resolve().parent
-SECRET_FILE = SECRET_PATH / 'db.json'
-secrets = json.loads(open(SECRET_FILE).read())
 
 def mongodb_connect(properties):
     pass
