948956f26802c1e4296a3de4b6bbbb1436af1179
jungwon-moon <jwoung0@naver.com>
2022-05-27 00:22:16 +0900

    check_trading_day 수정

--- qm/utils.py
+++ qm/utils.py
@@ -21,24 +21,25 @@ def check_trading_day(Dd, Type="API"):
     '''
     Return 
     False: non trading day
-    Dd: trading day
+    True: trading day
     '''
     Dd = dt2str(Dd)
-    diff = datetime.timedelta(days=1)
 
-    # 주말 확인
+    ### 주말 확인
+    # 0:월요일 ~ 6:일요일
     if datetime.date(int(Dd[:4]), int(Dd[4:6]), int(Dd[6:])).weekday() > 4:
-        return check_trading_day(str2dt(Dd) - diff, Type)
+        return False
 
-    # 휴장일 확인
+    ### 휴장일 확인
+    # API로 접근
     if Type == "API":
         req_url = 'http://quantmag.net/api/kr/holiday'
         holiday = [row['calnd_dd'] for row in requests.get(req_url).json()['results']]
         if Dd in holiday:
-            return check_trading_day(str2dt(Dd) - diff, Type)
+            return False
+    # DB로 접근
     else:
         holiday = [k[0] for k in Type.readDB('holiday', 'calnd_dd')]
         if Dd in holiday:
-            return check_trading_day(str2dt(Dd) - diff, Type)
-
-    return Dd
+            return False
+    return True
